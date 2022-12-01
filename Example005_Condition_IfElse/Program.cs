37a9554f541c77ae4431ed2d2e0de42b704d2077
Sergey <skarpov@gmail.com>
2022-12-01 20:10:39 +0300

    Added Example005_Condition_IfElse

---
+++ Example005_Condition_IfElse/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура! Привет, Маша!");
+}
+else
+{
+    Console.Write("Здарова, "); Console.Write(username); Console.WriteLine("!");
+}
