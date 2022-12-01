c408d059eddc2d2285fcddd4f5fb66c61ea0505a
Sergey <skarpov@gmail.com>
2022-12-01 19:16:53 +0300

    Edited Example003_Sum: added Random operator

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,4 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,10); //случайное число в диапазоне 1 - 10
+int numberB = new Random().Next(1,10);
 int sum = numberA + numberB;
-Console.WriteLine(sum);
+Console.Write (numberA); Console.Write(" + "); Console.Write (numberB); Console.Write (" = "); Console.WriteLine(sum);
